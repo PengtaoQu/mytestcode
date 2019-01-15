4e5f956c4432aabc70ef1701fbe11fe794735235
Pengtao Qu <pengtao.qu@atalent.com>
2019-01-15 21:26:18 +0800

    新添加的

---
+++ MigrationEf/Migrations/20190115053943_init.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MigrationEf.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "courses",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_courses", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "students",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_students", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "courses");
+
+            migrationBuilder.DropTable(
+                name: "students");
+        }
+    }
+}
---
+++ MigrationEf/model.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MigrationEf
+{
+    public class course
+    {
+
+         public int ID { get; set; }
+
+        public String Name { get; set; }
+
+    }
+
+
+    public class student
+    {
+
+        public int ID { get; set; }
+
+        public String Name { get; set; }
+
+    }
+
+    public class db : DbContext
+    {
+
+        public DbSet<course> courses { get; set; }
+
+        public DbSet<student> students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //注入Sql链接字符串
+            optionsBuilder.UseSqlServer(@"Server=.;Database=Test2;Trusted_Connection=True;");
+        }
+    }
+    public class db1 : DbContext
+    {
+
+        public DbSet<course> courses { get; set; }
+
+        public DbSet<student> students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //注入Sql链接字符串
+            optionsBuilder.UseSqlServer(@"Server=.;Database=Test2;Trusted_Connection=True;");
+        }
+    }
+
+}
